763d0e8a1ae3e1849a161a2b5339dc962f29fa50
Can1903a <150174478+Can1903a@users.noreply.github.com>
2023-12-05 22:12:00 +0300

    Add files via upload

---
+++ PHP-Project/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>E-Ticaret Sitesi</title>
+</head>
+<body>
+<?php include 'common.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">E-Ticaret</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/index.html">Ana Sayfa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/urunler.php">Ürünler</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/sepet.php">Sepet</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/hakkında.php">Hakkında</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/iletişim.php">İletişim</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/login.php">Giriş Yap</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/singing.php">Kayıt Ol</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </body>
+</html>
